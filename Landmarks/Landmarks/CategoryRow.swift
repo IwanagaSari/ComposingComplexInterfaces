38d83408bb8a5db16d00c6fd75329f1236546e7a
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-08-21 12:40:03 +0900

    Section3 の途中

--- Landmarks/Landmarks/CategoryRow.swift
+++ Landmarks/Landmarks/CategoryRow.swift
@@ -13,10 +13,20 @@ struct CategoryRow: View {
     var items: [Landmark]
     
     var body: some View {
-        HStack(alignment: .top, spacing: 0) {
-            ForEach(self.items) { landmark in
-                Text(landmark.name)
+        VStack(alignment: .leading) {
+            Text(self.categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(self.items) { landmark in
+                        Text(landmark.name)
+                    }
+                }
             }
+            .frame(height: 185)
         }
     }
 }
